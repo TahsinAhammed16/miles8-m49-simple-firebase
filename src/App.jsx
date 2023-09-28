5ad77d3abf4733f28a5bd9fab85e48a246ae448e
TahsinAhammed16 <tahsin.ahammed16@gmail.com>
2023-09-28 09:36:16 +0600

    react router, tailwind css setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-600 text-white font-bold text-2xl p-3 text-center'>React Firebase and Authentication</h1>
+    </>
+  )
+}
+
+export default App
