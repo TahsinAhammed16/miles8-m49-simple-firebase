a98ae78e1a7cad983c6b830b8fdc5d6adc96e923
TahsinAhammed16 <tahsin.ahammed16@gmail.com>
2023-09-28 10:12:34 +0600

    adding router and install firebase

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="bg-green-600 text-white font-bold text-2xl p-3 text-center">
+        React Firebase and Authentication
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9ueN46TScy8C5X0REn2Tiijbj7bdOG94",
+  authDomain: "miles8-m49-simple-firebase.firebaseapp.com",
+  projectId: "miles8-m49-simple-firebase",
+  storageBucket: "miles8-m49-simple-firebase.appspot.com",
+  messagingSenderId: "907384438417",
+  appId: "1:907384438417:web:d1061b3471aee943e50930"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
